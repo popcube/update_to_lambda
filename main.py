97e09a2040c12f491fb28a93b1e25bcc4a447ebe
popcube <40394657+popcube@users.noreply.github.com>
2023-10-24 00:16:47 +0900

    scheduled

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import boto3
 import requests
+import sys
 
 client = boto3.client('lambda')
 
@@ -10,6 +11,7 @@ def get_token_from_texts(token_name, r_text):
 
 
 def main():
+    gt = ""
     r = requests.get("https://twitter.com/elonmusk/status/1692431667548528661")
 
     if not r.ok:
@@ -17,10 +19,13 @@ def main():
         print(r.status_code)
         # print(r.headers)
         print(r.text)
-        # sys.exit(1)
+        sys.exit(1)
 
     gt = get_token_from_texts("gt", r.text)
     print("gt: " + gt)
+    if len(gt) == 0:
+        print("gt could not be acquired")
+        sys.exit(1)
 
     client.update_function_configuration(
         FunctionName='Twitter_frontend_multi_acc_dynamodb',

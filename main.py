8435bb96365844125b5378e2f5dd4b062fab4d50
popcube <40394657+popcube@users.noreply.github.com>
2023-11-02 22:01:22 +0900

    activate url adoption

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 import boto3
 import requests
 import sys
+import json
 
 client = boto3.client('lambda')
 
-
-def get_token_from_texts(token_name, r_text):
-    token_index = r_text.find(token_name + "=")
-    if token_index == -1:
-        print("token cannot be found")
-        sys.exit(1)
-    return r_text[token_index+len(token_name + "="): r_text.find(";", token_index)]
-
+activate_url = "https://api.twitter.com/1.1/guest/activate.json"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
 
 def main():
     gt = ""
-    r = requests.get("https://twitter.com/elonmusk/status/1692431667548528661")
+
+    header_dic = {
+        "Authorization": "Bearer " + bearer_token
+    }
+    r = requests.post(activate_url,headers=header_dic)
 
     if not r.ok:
-        print("elonmusk")
+        print("activate url post failed")
         print(r.status_code)
         # print(r.headers)
         print(r.text)
         sys.exit(1)
-
-    gt = get_token_from_texts("gt", r.text)
+    
+    try:
+        gt = json.loads(r.text)["guest_token"]
+    except Exception as e:
+        print("gt was not included in reponse from activate url")
+        print()
+        print(e)
     print("gt: " + gt)
-    if len(gt) == 0:
-        print("gt could not be acquired")
-        sys.exit(1)
 
     client.update_function_configuration(
         FunctionName='Twitter_frontend_multi_acc_dynamodb',
